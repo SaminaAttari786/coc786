00c91daf2f3b1d5a3fc2675a401a799bb30da57c
Samina Attari <saminaattari7860@yahoo.com>
2020-07-03 21:43:46 +0530

    Initial commit

---
+++ Batch1/src/main/java/com/example/batch1/Constants1.java
@@ -0,0 +1,6 @@
+package com.example.batch1;
+
+public class Constants1 {
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
